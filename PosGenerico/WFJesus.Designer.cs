f2b9525db031a598ba6f49b6fda4ca695911bf70
Jesús Sánchez <39342586+jesus-sj@users.noreply.github.com>
2020-01-24 12:36:40 -0600

    Al hacer click en el nombre jesus despliega un messageBox

--- PosGenerico/WFJesus.Designer.cs
+++ PosGenerico/WFJesus.Designer.cs
@@ -36,9 +36,10 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(113, 124);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.Size = new System.Drawing.Size(39, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Jesus";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // WFJesus
             // 
--- PosGenerico/WFJesus.cs
+++ PosGenerico/WFJesus.cs
@@ -16,5 +16,22 @@ namespace PosGenerico
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Initializes the variables to pass to the MessageBox.Show method.
+            string message = "You did not enter a server name. Cancel this operation?";
+            string caption = "Error Detected in Input";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            // Displays the MessageBox.
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                // Closes the parent form.
+                this.Close();
+            }
+        }
     }
 }
